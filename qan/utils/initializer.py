5f0885f8e1df88d38a3b657d702e79d435e2bb59
rifki2 <rifki@nodeflux.io>
2021-06-28 16:26:38 +0700

    add new initializer

--- qan/utils/initializer.py
+++ qan/utils/initializer.py
@@ -1,4 +1,5 @@
 from easydict import EasyDict as edict
+from importlib import import_module
 
 __all__ = ["initialize_dataset", "initialize_optimizer", "initialize_model"]
 
@@ -24,3 +25,31 @@ def initialize_optimizer(module: object, method: str):
 
     optimizer = getattr(module, method)
     return optimizer
+
+def create_instance(module_cfg: edict, **kwargs):
+    module = module_cfg.module
+    method = module_cfg.method
+    module_args = module_cfg.args
+    module_args.update(kwargs)
+    
+    print(f"Initializing {module}.{method}")
+    module = import_module(module)
+    module = getattr(module, method)
+    
+    try:
+        instance = module(**module_args)
+    except:
+        instance = [module(module_args)]
+        
+    return instance
+
+def initialize_tokenizer(module_cfg: edict, **kwargs):
+    module = module_cfg.module
+    method = module_cfg.method
+    module_args = module_cfg.args
+    print(f"Initializing {module}.{method}")
+    
+    module = import_module(module)
+    method_obj = getattr(module, method)
+    return method_obj.from_pretrained(**module_args)
+    
