95d09d31d99528991ac8b8cfab9d9a7a4e086a7a
rifki2 <rifki@nodeflux.io>
2021-06-29 18:02:12 +0700

    add transformer model

--- qan/models.py
+++ qan/models.py
@@ -10,8 +10,12 @@ import torch.nn.functional as F
 import torch
 from torch import nn
 from torch.nn.init import xavier_uniform
+from transformers.tokenization_utils_base import BatchEncoding
 
-        
+    
+# -----------------------------------
+# Seq2Seq RNN
+# -----------------------------------
 class Seq2SeqQA(pl.LightningModule):
     def __init__(self, 
                  model_cfg: edict,        # model configuration
@@ -88,9 +92,6 @@ class Seq2SeqQA(pl.LightningModule):
                  
         return encodings, answers_start_positions, answers_end_positions    
 
-
-    def _preprocess_val_batch(self, batch):
-        return self._preprocess_train_batch(batch)
     
     def _preprocess_inference(self, batch):
         batch = edict(batch)
@@ -100,23 +101,24 @@ class Seq2SeqQA(pl.LightningModule):
                                    padding=True)
         encodings_ids = torch.tensor(encodings['input_ids'], dtype=torch.int32, device=self.device)
         
-        return encodings_ids
+        return encodings_ids, batch.context, encodings
     
     @torch.no_grad()
     def predict(self, x):
-        encodings_ids = self._preprocess_inference(x)
+        encodings_ids, gold_context, encodings = self._preprocess_inference(x)
         out = self(inputs=encodings_ids)
         
         logits_start, logits_end = out
         predictions_start, predictions_end = F.softmax(logits_start, dim=1), F.softmax(logits_end, dim=1) # (batch_size, num_classes/max_sequence) 
         
-        return self.postprocess((predictions_start, predictions_end), contexts_ids=encodings_ids)
+        return self.postprocess((predictions_start, predictions_end), gold_context=gold_context, encodings=encodings)
     
     def postprocess(self, 
-                    x: set, 
-                    contexts_ids: List[List]) -> List:
+                    start_end_pair: set, 
+                    gold_context: list,
+                    encodings: BatchEncoding) -> List:
         # get confidence and labels
-        predictions_start, predictions_end = x
+        predictions_start, predictions_end = start_end_pair
         confs_start, preds_start = torch.max(predictions_start, dim=1)
         confs_end, preds_end = torch.max(predictions_end, dim=1)
         
@@ -127,43 +129,31 @@ class Seq2SeqQA(pl.LightningModule):
         outputs = list()    
         for i in range(len(preds_start)):
             start = preds_start[i]
-            end = preds_end[i]
-            ans = self.tokenizer.decode(contexts_ids[i][start:end+1])
+            end = preds_end[i]  
+            
+            start_str_span = encodings.token_to_chars(i, start) # CharSpan
+            end_str_span = encodings.token_to_chars(i, end)     # CharSpan
+            
+            ans = gold_context[i][start_str_span.start:end_str_span.end]
             outputs.append(ans)
 
         return outputs
     
     def forward(self, 
-                inputs: torch.Tensor,
-                # context: torch.Tensor, 
-                # question: torch.Tensor, 
-                teacher_forcing_ratio: float = 0.5):
+                inputs: torch.Tensor):
         """[pass forward method of the model]
 
         Args:
-            context (torch.Tensor): [context tensor, shape: batch_size, padded_variable_len]
-            question (torch.Tensor): [question tensor, shape: batch_size, padded_variable_len]
-            teacher_forcing_ratio (float, optional): [threshold to use ground truth as decoder inputs]. 
-                                                      Defaults to 0.5.
+            inputs (torch.Tensor): [concatination of context and question ids, shape: batch_size, padded_variable_len]
+
 
         Returns:
             [type]: [description]
         """
         
-        # assert len(context.size()) == 2
-        # assert len(question.size()) == 2
-        
-        # set to 2 due to predict the start,end over possibles location in the context
-        
         # encode context and question
         out_context, _, _ = self.encoder(inputs) # n_layers * n_directions(2 for bidirectional), bs, hid_dim
                 
-        # concate the context and question hidden states
-        # n_layers*n_directions, b_size, h_dim --> b_size, n_layers*n_directions*h_dim
-        # context_hidden = question_hidden.permute(1,0,2).flatten(start_dim=1)
-        # question_hidden = question_hidden.permute(1,0,2).flatten(start_dim=1)
-        
-        # hidden_state_cat = torch.cat((context_hidden, question_hidden), dim=1) # b_size, 2*(n_layers*n_directions*h_dim)
         logits = self.classifier(out_context) # batch_size, max_sequence, num_classes==2
         start_logits, end_logits = logits.split(1, dim=-1) # batch_size, max_sequence, 1
         start_logits = start_logits.squeeze(-1).contiguous()  # (bs, max_query_len)
@@ -206,30 +196,36 @@ class Seq2SeqQA(pl.LightningModule):
         self.log('train_loss', loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
         return loss
 
+    def _get_answers_token_positions(self, 
+                             context_encodings, 
+                             answers: edict):
+        start_positions = []
+        end_positions = []
+        
+        answer_start = answers.answer_start[0]
+        answer_end = answers.answer_end[0]
+        answer_text = answers.text[0]
+        
+        assert isinstance(answer_start, torch.Tensor)
+        assert isinstance(answer_end, torch.Tensor)
+        
+        for i in range(len(answer_text)):
+            start = context_encodings.char_to_token(i, answer_start[i])
+            end = context_encodings.char_to_token(i, answer_end[i] - 1)
 
-    def validation_step(self, batch, batch_idx):
-        pass
- 
+            start_positions.append(start)
+            end_positions.append(end)            
 
-    def test_step(self, batch):
-        pass
-                
-    def validation_epoch_end(self, outs):
-        # self.log_dict(self.metrics.compute(), on_epoch=True, prog_bar=True, logger=True)
-        # # we know that ClassificationMetrics' state need to be reset
-        # self.metrics.eval_init()
-        pass
+            # if start position is None, the answer passage has been truncated
+            if start_positions[-1] is None:
+                start_positions[-1] = self.tokenizer.model_max_length
+                start = self.tokenizer.model_max_length
+            if end_positions[-1] is None:
+                end_positions[-1] = self.tokenizer.model_max_length
+                end = self.tokenizer.model_max_length
+        
+        return torch.tensor(start_positions), torch.tensor(end_positions) 
 
-    def training_epoch_end(self, outs):
-        # # log epoch metric
-        # self.log_dict(self.metrics.compute())
-        # # we know that ClassificationMetrics' state need to be reset
-        # self.metrics.eval_init()
-        pass
-                   
-    def configure_optimizers(self):
-        return self.optimizer(self.parameters(), **self.hparams.optimizer_cfg.args)
-    
     def _load_pretrained_embedding(self, 
                                    pretrained_cfg: edict, 
                                    embedding_dim: int,
@@ -249,6 +245,181 @@ class Seq2SeqQA(pl.LightningModule):
                 pretrained_tensor[id, :] = vec.unsqueeze(0)
                 
         return pretrained_tensor
+
+    def configure_optimizers(self):
+        return self.optimizer(self.parameters(), **self.hparams.optimizer_cfg.args)
+
+
+    
+
+# -----------------------------------
+# Transformers-based QA Model
+# -----------------------------------
+class TransformersQA(pl.LightningModule):
+    def __init__(self, 
+                 model_cfg: edict,        # model configuration
+                 optimizer_cfg: edict,    # opitimizer configuration
+                 tokenizer_cfg: edict,    # tokenizer configuration
+                ):
+        super().__init__()
+
+        # save hyperparameters
+        self.save_hyperparameters()
+        self.model_cfg = model_cfg
+        
+        # ------------
+        # Training Building Block
+        # ------------    
+        self.tokenizer = initialize_tokenizer(tokenizer_cfg)
+        self.vocab = edict(self.tokenizer.get_vocab())
+        self.vocab_size = self.tokenizer.vocab_size 
+        self.max_sequence = self.tokenizer.model_max_length
+        self.optimizer = initialize_optimizer(torch.optim, optimizer_cfg.module)
+        self.criterion = nn.CrossEntropyLoss(ignore_index=self.max_sequence)
+        
+        # init model
+        self._init_model()
+    
+    def _init_model(self):
+        # ------------
+        # Backbone
+        # ------------
+        encoder_config = self.model_cfg.encoder.config
+        self.tranformer_config = create_instance(encoder_config)
+        self.encoder = create_instance(config=self.tranformer_config)
+        
+        # ------------
+        # Classifier
+        # ------------
+        self.classifier = SimpleMLP(in_features=encoder_config.args.hidden_size,
+                                    out_features=2)
+        
+        
+    def _preprocess_train_batch(self, batch):
+        
+        batch = edict(batch)
+        answers = batch.answers
+        encodings = self.tokenizer(batch.context, 
+                                   batch.question,
+                                   truncation=True,
+                                   padding=True)
+        
+        # get answers start/end positions in encoding
+        answers_start_positions, answers_end_positions = self._get_answers_token_positions(context_encodings=encodings, 
+                                                                                           answers=answers)
+                 
+        return encodings, answers_start_positions, answers_end_positions    
+
+    
+    def _preprocess_inference(self, batch):
+        batch = edict(batch)
+        encodings = self.tokenizer(batch.context, 
+                                   batch.question,
+                                   truncation=True,
+                                   padding=True)
+        encodings_ids = torch.tensor(encodings['input_ids'], dtype=torch.int32, device=self.device)
+        attention_mask = torch.tensor(encodings['attention_mask'], dtype=torch.int32, device=self.device)
+        
+        return (encodings_ids, attention_mask), batch.context, encodings
+    
+    @torch.no_grad()
+    def predict(self, x):
+        encodings_ids, attention_mask, gold_context, encodings = self._preprocess_inference(x)
+        
+        # pass forward the model
+        out = self(input_ids = encodings_ids,
+                   attention_mask = attention_mask)
+        
+        logits_start, logits_end = out
+        predictions_start, predictions_end = F.softmax(logits_start, dim=1), F.softmax(logits_end, dim=1) # (batch_size, num_classes/max_sequence) 
+        
+        return self.postprocess((predictions_start, predictions_end), gold_context=gold_context, encodings=encodings)
+    
+    def postprocess(self, 
+                    start_end_pair: set, 
+                    gold_context: list,
+                    encodings: BatchEncoding) -> List:
+        # get confidence and labels
+        predictions_start, predictions_end = start_end_pair
+        confs_start, preds_start = torch.max(predictions_start, dim=1)
+        confs_end, preds_end = torch.max(predictions_end, dim=1)
+        
+        # convert to numpy tensor
+        confs_start, preds_start = confs_start.cpu().numpy(), preds_start.cpu().numpy()
+        confs_end, preds_end = confs_end.cpu().numpy(), preds_end.cpu().numpy() 
+        
+        outputs = list()    
+        for i in range(len(preds_start)):
+            start = preds_start[i]
+            end = preds_end[i]  
+            
+            start_str_span = encodings.token_to_chars(i, start) # CharSpan
+            end_str_span = encodings.token_to_chars(i, end)     # CharSpan
+            
+            ans = gold_context[i][start_str_span.start:end_str_span.end]
+            outputs.append(ans)
+
+        return outputs
+    
+    def forward(self, 
+                input_ids: torch.Tensor,
+                attention_mask: torch.Tensor):
+        """[pass forward method of the model]
+
+        Args:
+            input_ids (torch.Tensor): [concatination of context and question ids, shape: (batch_size, max_sequence)]
+            attention_mask (torch.Tensor): []
+        Returns:
+            start_logits[torch.Tensor]: [logits of start indexes predictions, shape: (batch_size, max_sequence)]
+            end_logits[torch.Tensor]: [logits of end indexes predictions, shape: (batch_size, max_sequence)]
+        """
+                
+        # encode context and question
+        outputs = self.encoder(input_ids = input_ids,
+                               attention_mask = attention_mask) 
+        last_hidden_state = outputs[0] 
+        
+        logits = self.classifier(last_hidden_state) # batch_size, max_sequence, num_classes==2
+        start_logits, end_logits = logits.split(1, dim=-1) # batch_size, max_sequence, 1
+        start_logits = start_logits.squeeze(-1).contiguous()  # (bs, max_query_len)
+        end_logits = end_logits.squeeze(-1).contiguous()  # (bs, max_query_len)
+        
+        return start_logits, end_logits
+    
+    def training_step(self, batch: set, batch_idx):
+        """[training step]
+
+        Args:
+            batch ([set]): [input, target, length input]
+            batch_idx ([type]): [description]
+
+        Returns:
+            loss[float]: [loss value]
+        """        
+        encodings, answers_start_positions, answers_end_positions  = self._preprocess_train_batch(batch)
+        encodings_ids = torch.tensor(encodings['input_ids'], dtype=torch.int32, device=self.device)
+        attention_mask = torch.tensor(encodings['attention_mask'], dtype=torch.int32, device=self.device)
+        
+        # pass forward the model
+        start_logits, end_logits = self(input_ids = encodings_ids,
+                                        attention_mask = attention_mask)
+        
+        # calculate loss value
+        start_logits = start_logits.squeeze(-1).contiguous()  # (batch_size, class)
+        end_logits = end_logits.squeeze(-1).contiguous()  # (batch_size, class)
+        
+        # move targets to device
+        answers_start_positions = answers_start_positions.to(self.device)
+        answers_end_positions = answers_end_positions.to(self.device)
+        
+        # measure loss
+        start_loss = self.criterion(start_logits, answers_start_positions)
+        end_loss = self.criterion(end_logits, answers_end_positions)
+        loss = (start_loss + end_loss) / 2
+
+        # log loss
+        self.log('train_loss', loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        return loss
 
     def _get_answers_token_positions(self, 
                              context_encodings, 
@@ -278,4 +449,7 @@ class Seq2SeqQA(pl.LightningModule):
                 end_positions[-1] = self.tokenizer.model_max_length
                 end = self.tokenizer.model_max_length
         
-        return torch.tensor(start_positions), torch.tensor(end_positions)
+        return torch.tensor(start_positions), torch.tensor(end_positions) 
+
+    def configure_optimizers(self):
+        return self.optimizer(self.parameters(), **self.hparams.optimizer_cfg.args)
